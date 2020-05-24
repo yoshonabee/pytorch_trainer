d0c5ea7dee411dd771c3d5f473a5ec5cff38e282
yoshonabee <jhann0120@gmail.com>
2020-05-24 14:08:50 +0800

    <feat>: add precision, recall, f1-score to metrics

--- metrics.py
+++ metrics.py
@@ -27,3 +27,55 @@ class Accuracy(BaseMetric):
 
     def __call__(self, pred, target):
         return (pred.max(-1)[1] == target).float().mean().item()
+
+class Precision(BaseMetric):
+    name= 'precision'
+
+    def __init__(self, *args, **kwargs):
+        super(Accuracy, self).__init__(*args, **kwargs)
+
+    def __call__(self, pred, target):
+        tp, fn, fp, tn = classification_measurements(pred, target)
+        return tp / (tp + fp)
+
+class Recall(BaseMetric):
+    name= 'recall'
+
+    def __init__(self, *args, **kwargs):
+        super(Accuracy, self).__init__(*args, **kwargs)
+
+    def __call__(self, pred, target):
+        tp, fn, fp, tn = classification_measurements(pred, target)
+        return tp / (tp + fn)
+
+class F1(BaseMetric):
+    name= 'f1-score'
+
+    def __init__(self, *args, **kwargs):
+        super(Accuracy, self).__init__(*args, **kwargs)
+
+    def __call__(self, pred, target):
+        tp, fn, fp, tn = classification_measurements(pred, target)
+        return 2 * tp / (2 * tp + fn + fp)
+
+def classification_measurements(pred, target):
+    pred = pred.detach().cpu().max(-1)[1].view(-1).numpy()
+    target = target.detach().cpu().view(-1).numpy()
+
+    tp, fn, fp, tn = 0, 0, 0, 0
+
+    for p, t in zip(pred, target):
+        if t:
+            if p:
+                tp += 1
+            else:
+                fn += 1
+        else:
+            if p:
+                fp += 1
+            else:
+                tn += 1
+    
+    return tp, fn, fp, tn
+
+
